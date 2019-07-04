dbc160187fdea8a6991221918a896864f6bd39dc
Julian <juliandave1989@gmail.com>
2019-07-05 01:02:10 +0300

    added 'Page not found' route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { ShowTodosComponent } from './components/show-todos/show-todos.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  {path:"", component:ShowTodosComponent, pathMatch:'full'},
-  {path:"add", component:AddTodoComponent, pathMatch:'full'}
+  {path:"", component:ShowTodosComponent},
+  {path:"add", component:AddTodoComponent},
+  { path: '**', component:PageNotFoundComponent}
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 
-import { ShowTodosComponent } from './components/show-todos/show-todos.component'
+import { ShowTodosComponent } from './components/show-todos/show-todos.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     AddTodoComponent,
     ShowTodosComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
