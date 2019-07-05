2a93e3edcecc99d901db548cc4ee3ff4a10b1177
Julian <juliandave1989@gmail.com>
2019-07-05 22:48:38 +0300

    added service error handler

--- client/src/app/services/todos.service.ts
+++ client/src/app/services/todos.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ITodo } from '../models/todo.model'
 import { ISubject } from '../models/subjects.model'
+import { catchError } from 'rxjs/operators';
+import { throwError } from "rxjs";
+
+
 
 const httpOptions: any = {
   headers: new HttpHeaders({
@@ -20,24 +24,45 @@ export class TodosService {
 
   constructor(private http: HttpClient) { }
 
+  private handleError(errorResponse: HttpErrorResponse) {
+    if (errorResponse.error instanceof ErrorEvent) {
+      console.error("Client Side Error: ", errorResponse.error.message)
+    }
+    else console.error("Server Side Error: ", errorResponse)
+
+    return throwError("There is a problem with the service. We are notified & working on it. Please try again later.")
+  }
+
   getTodos(): Observable<ITodo[]> {
-    return this.http.get<ITodo[]>('/api/todo');
+    return this.http.get<ITodo[]>('/api/todo')
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   getSubjects(): Observable<ISubject[]> {
-    return this.http.get<ISubject[]>('/api/subject');
+    return this.http.get<ISubject[]>('/api/subject')
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   addTodo(ev: ITodo): Observable<Object> {
-    return this.http.post<ITodo>('api/todo',
+    return this.http.post('api/todo',
       {
         Description: ev.Description,
         subject: ev.subject
       },
-      httpOptions);
+      httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   deleteTodo(id: string): Observable<Object> {
-    return this.http.delete(`api/todo/${id}`);
+    return this.http.delete(`api/todo/${id}`)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 }
